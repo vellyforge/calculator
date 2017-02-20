ff71b3e86391a3827806463fcabfffbdce355b4f
vellyforge <epsilon@MacBook-Air13.local>
2017-02-20 19:37:59 +0300

    First master

---
+++ calculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by epsilon on 20.02.17.
+//  Copyright © 2017 epsilon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayResultLabel: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        let number = sender.currentTitle!
+        print (number)
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
+
